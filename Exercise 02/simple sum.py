3f3093a7eda8b53ad85577fd033b485c85d2cbeb
shahwaiz087 <shahwaizimran087@gmail.com>
2024-10-23 15:00:10 +0400

    simplesum.py
    simple sum.py

---
+++ Exercise 02/simple sum.py
@@ -0,0 +1,18 @@
+#Steps:
+#1 Create a variable and set its initial value to the whole number '8'.
+#2 Create a second variable and set its initial value to the whole number '10'.
+#3 Assign the total of the first two integers to a third variable.
+#4 Output the total sum value to the console.
+
+#simple sums
+#adding a number to the first variable
+balloon = (int(8))
+
+#adding a number to the second variable
+house = (int(10))
+
+#adding both the first and second variable to the third variable
+country = (balloon + house)
+
+#print the variable city
+print(country)
