28bf0e3198168b29ebbec0b1fdffbb02cd26bc52
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-06 15:02:33 +0400

    primitivequiz.py
    primitive quiz

---
+++ Exercise 04/Primitivequiz.py
@@ -0,0 +1,11 @@
+#steps
+#1 Create a program that asks the user, "What is the capital of France?" and then waits for their response.
+#2 If they type the correct response ("Paris"), the program should show that the message entered is correct.
+#3 It would print incorrectly if the response was incorrect.
+
+#applying the variable question.
+Answer = input("What is the capital of France?:")
+if Answer =="Paris" : #inserting the answer to the question
+    print("the answer is correct") #if the answer is Paris then it will be correct
+else:
+    print("the answer is wrong.") #If the answer will be wrong it won't be paris 
