41f809ff31bc47cbea567b77111bf38cf3b163a8
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-22 23:27:06 +0400

    Optional Requirement.py

--- Exercise 03/Optional Requirement.py
+++ Exercise 03/Optional Requirement.py
@@ -1,6 +1,3 @@
-# Request that the user enter their entire name, finishing any unnecessary spaces
-user_name = input("Enter your full name: ").strip() # .strip() eliminates unnecessary gaps surrounding the input
-
 # Remove any unnecessary whitespace and ask the user to provide their hometown
 user_hometown = input("Enter your hometown: ").strip() # .strip() Eliminates unnecessary gaps surrounding the input
 
@@ -12,7 +9,7 @@ while True:
         break  # If conversion is successful, end the loop
     except ValueError:
         # If an invalid integer is entered, display an error
-        print("Invalid input. Please enter a numerical value for age.")
+        print("Invalid input. Enter a numerical value for your age.")
 
 # Show the collected data
 print("\nUser Information:")
