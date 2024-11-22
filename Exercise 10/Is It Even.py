69a8da3797977a7bb8d780a4a7b2d819721f50c6
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-22 23:51:45 +0400

    Is It Even.py
    Is It Even.py

--- Exercise 10/Is It Even.py
+++ Exercise 10/Is It Even.py
@@ -1,25 +1,25 @@
-# A function to determine whether a given number is odd or even
+#A function to determine whether a given number is odd or even
 def check_even_odd(number):
-    # The integer is even if it is evenly divisible by two 
+    #The integer is even if it is evenly divisible by two 
     if number % 2 == 0:
         return f"{number} is even."
     else:
-        # Otherwise, it's odd
+        #Otherwise, it's odd
         return f"{number} is odd."
 
-# The main purpose is to process user input and display the outcome
+#The main purpose is to process user input and display the outcome
 def main():
-    # Request the user to enter a number
+    #Request the user to enter a number
     try:
         user_number = int(input("Please enter a number: "))
-        # Get the outcome message by calling the check_even_odd function
+        #Get the outcome message by calling the check_even_odd function
         result_message = check_even_odd(user_number)
-        # Print the message of the result
+        #Print the message of the result
         print(result_message)
     except ValueError:
-        # Show an error message if the user inputs a value that isn't an integer
+        #Show an error message if the user inputs a value that isn't an integer
         print("Invalid input. Enter a value in numbers.")
 
-# Run this script straight alone to execute the main function
+#Run this script straight alone to execute the main function
 if __name__ == "__main__":
     main()
