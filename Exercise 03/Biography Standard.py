d92bb350893bf647c5e659e30a0b07022d4d8397
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-22 23:59:49 +0400

    Biography Standard.py
    Biography Standard.py

--- Exercise 03/Biography Standard.py
+++ Exercise 03/Biography Standard.py
@@ -1,14 +1,14 @@
-# Give personal information variables
-name = "Shahwaiz"  # Name in a string
-hometown = "Pakistan"    # The string "hometown"
-age = 18                 # An integer "age"
+#Give personal information variables
+name = "Shahwaiz"  #Name in a string
+hometown = "Pakistan"    #The string "hometown"
+age = 18                 #An integer "age"
 
-# Use descriptive keys to keep material organized in a dictionary
+#Use descriptive keys to keep material organized in a dictionary
 info = {
-    "Name": name,        # The value of "Name" in the key
-    "Hometown": hometown, # "Hometown" as a key with hometown value
-    "Age": age            # The value of "Age" in the key
+    "Name": name,        #The value of "Name" in the key
+    "Hometown": hometown, #"Hometown" as a key with hometown value
+    "Age": age            #The value of "Age" in the key
 }
 
-# Every piece of information should be printed on a separate line
+#Every piece of information should be printed on a separate line
 print(f"{info['Name']}\n{info['Hometown']}\n{info['Age']}")
