c0eb06c1f18f7ace421720d9f36d3787198a12a2
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-25 11:45:53 +0400

    List Names.py
    List Names.py

---
+++ Z-Bonus.py/List names.py
@@ -0,0 +1,8 @@
+# Keep a record of the names of your friends
+names = ["Farah", "Suleiman", "Raza", "Masab"]
+
+# One at a time, print each person's name
+print(names[0])  #First name
+print(names[1])  #Second name
+print(names[2])  #Third name
+print(names[3])  #Fourth name
