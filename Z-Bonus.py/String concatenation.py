6f6d1f310bf7400958e761c549e85f36c9392b97
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-25 11:48:18 +0400

    String Concatenation.py
    String Concatenation.py

---
+++ Z-Bonus.py/String concatenation.py
@@ -0,0 +1,8 @@
+# It is necessary to define three strings in different variables
+string1 = "The"
+string2 = "End"
+string3 = "Near"
+
+#Combine the strings, then output the result
+Result = string1 + " " + string2 + " " + string3
+print(Result)
