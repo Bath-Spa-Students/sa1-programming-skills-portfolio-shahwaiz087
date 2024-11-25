ffe94e269b9ab76c706f5e365738d1fc7e41500c
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-25 11:42:54 +0400

    Control Structure.py
    Control Structure.py

---
+++ Z-Bonus.py/Control structure.py
@@ -0,0 +1,16 @@
+#Test (Pass):
+
+# Determine the alien's color
+alien_color = 'Red'
+
+# Check to see if the alien is red
+if alien_color == 'Red':
+    print("Five points have just been awarded to the player.")
+
+#Test (Fail):
+
+# Determine the alien's color
+alien_color = 'Red'
+# Check to see if the alien is red
+if alien_color == 'Yellow':
+    print("Five points have just been awarded to the player.")
