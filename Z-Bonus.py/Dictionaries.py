8caf46501d3409cb3318860460eec60de45254e7
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-25 11:43:43 +0400

    Dictionaries.py

---
+++ Z-Bonus.py/Dictionaries.py
@@ -0,0 +1,17 @@
+#Keep a dictionary with details on people you know.Maintain a record of their first name
+#Full name, age and city of residence 
+#Keys like city, age, last name, and first name should be in your possession. Print all of the individual facts that are contained in your dictionary
+
+#The dictionary's development
+Person_information = {
+    "First name": "Shahwaiz",
+    "Last name": "Imran",
+    "Age": 18,
+    "City" : "Sharjah"
+}
+
+#Printing every item of data
+print(f"First Name: {Person_information['First name']}")
+print(f"Last name: {Person_information['Last name']}")
+print(f"Age: {Person_information['Age']}")
+print(f"City: {Person_information['City']}")
