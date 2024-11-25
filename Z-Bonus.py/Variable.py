c0e0bdb9594e38521a1e2a37495f513d0b1bbfa8
shahwaiz087 <shahwaizimran087@gmail.com>
2024-11-25 11:50:34 +0400

    Variable.py
    Variable.py

---
+++ Z-Bonus.py/Variable.py
@@ -0,0 +1,7 @@
+# Assign a variable a value
+Message = "Titanic is a good movie"
+print(Message)
+
+# Change the value of the variable to a new message
+Message = "Peaky Blinders is the best netflix show"
+print(Message)
